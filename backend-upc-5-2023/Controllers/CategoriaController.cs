0048da24686dff1b3382c94f6df893f6df3923c0
Rafael Heredia Padilla <rafaelherediap@gmail.com>
2023-06-21 19:03:17 -0400

    Update CategoriaController.cs

--- backend-upc-5-2023/Controllers/CategoriaController.cs
+++ backend-upc-5-2023/Controllers/CategoriaController.cs
@@ -88,15 +88,10 @@ namespace backend_upc_5_2023.Controllers
         {
             try
             {
-                var a = CategoriaServicios.ValidarCategoria(categoria);
-
-                if (0==0)
-                {
+                
                     var result = CategoriaServicios.Insert(categoria);
                     return Ok(result);
-                }
-                
-                    return StatusCode(500, "Categoria ya existe");
+               
             }
             catch (Exception ex)
             {
