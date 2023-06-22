168d685144c4025aee155f05c759524bc47e942e
Rafael Heredia Padilla <rafaelherediap@gmail.com>
2023-06-21 21:28:47 -0400

    Update Program.cs

--- backend-upc-5-2023/Program.cs
+++ backend-upc-5-2023/Program.cs
@@ -7,6 +7,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options => options.AddPolicy("DevelopmentCors",
+    builder => builder.AllowAnyMethod()
+                      .AllowAnyOrigin()
+                      .WithMethods("POST", "GET")
+                      .AllowAnyHeader()));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
